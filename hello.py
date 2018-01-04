37e326cde00f7f30f205f94b884304fd0270c1de
HanSangYoon <microscope83@gmail.com>
2018-01-04 10:54:25 +0900

    test modify_2018_0104

--- hello.py
+++ hello.py
@@ -1,13 +1,4 @@
 hello world!!!!
 hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
+hi world!!!!
+hi f***ing working world!!!!!!!!
