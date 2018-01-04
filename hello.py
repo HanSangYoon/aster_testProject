ef93ab1caabd56949959eddf18e7d42ebf2f0597
microscope83 <microscope83@naver.com>
2018-01-04 10:59:57 +0900

    hello.py modified on local repository

--- hello.py
+++ hello.py
@@ -3,11 +3,5 @@ hello world!!!!
 hello world!!!!
 hello world!!!!
 hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
-hello world!!!!
+//For command line git tutorial
+//Local repository modification
